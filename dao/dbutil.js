86d9f97767e5b4f08396f1b0d79dba136ceb0c99
layoung <184592775@qq.com>
2020-01-18 17:36:59 +0800

    a

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
 
 function createConnection() {
     let connection = mysql.createConnection({
-        host: '192.168.0.107',
+        host: '192.168.56.1',
         port: '3306',
         user: 'root',
         password: '184592',
